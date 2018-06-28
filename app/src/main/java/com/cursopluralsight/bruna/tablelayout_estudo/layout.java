b7687b0852b794be510fddca281175c7c745f9dd
BrunaTiba <brunaspinolatiba@gmail.com>
2018-06-28 16:55:23 -0300

    Estudando tablelayout

---
+++ app/src/main/java/com/cursopluralsight/bruna/tablelayout_estudo/layout.java
@@ -0,0 +1,13 @@
+package com.cursopluralsight.bruna.tablelayout_estudo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class layout extends AppCompatActivity {
+    cted void onCreate(Bundle savedInstanceState) {
+        supe
+    @Override
+    proter.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout);
+    }
+}
