a8e79c150132a04edd9471771245b42395a7995f
BrunaTiba <brunaspinolatiba@gmail.com>
2018-06-29 09:21:11 -0300

    Estudando Landscape

---
+++ Landscape_estudo/app/src/main/java/com/cursopluralsight/bruna/landscape_estudo/Landscape.java
@@ -0,0 +1,13 @@
+package com.cursopluralsight.bruna.landscape_estudo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Landscape extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landscape);
+    }
+}
